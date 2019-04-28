60cc2f9e74415790cf228eab2612f47e43aaa3e4
d.endeser1@gmail.com <d.endeser1@gmail.com>
2019-04-28 14:47:39 +0300

    Добавьте файлы проекта.

---
+++ L9_P_EX/Practice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9_P_EX
+{
+    class Practice
+    {
+
+    }
+}
