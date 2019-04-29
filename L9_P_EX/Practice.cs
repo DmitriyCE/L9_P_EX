4d1007cf29ac4aa7cb6ca402fdf1f8e673e9c505
d.endeser1@gmail.com <d.endeser1@gmail.com>
2019-04-29 11:47:35 +0300

    Homework_completed

--- L9_P_EX/Practice.cs
+++ L9_P_EX/Practice.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace L9_P_EX
 {
-    partial class Practice
+    public static class Practice
     {
         public struct Coordinates
         {
@@ -35,39 +35,21 @@ namespace L9_P_EX
             double vector =Math.Sqrt(x*x+y*y+z*z);
             Console.WriteLine(vector);
         }
-        public struct DateTime
-        {
-            public int year;
-            public int month;
-            public int day;
-            public int hours;
-            public int minutes;
-            public int seconds;
-            public DateTime(int year = 1, int month = 1, int day = 1, int hours=0, int minutes=0, int seconds=0)
-            {
-                this.year = year;
-                this.month = month;
-                this.day = day;
-                this.hours = hours;
-                this.minutes= minutes;
-                this.seconds= seconds;
-            }
-                void Deconstruct(out int year, out int month, out int day, out int hours, out int minutes, out int seconds)
-            {
-                year = this.year;
-                month = this.month;
-                day = this.day;
-                hours = this.hours;
-                minutes = this.minutes;
-                seconds = this.seconds;
-            }
-        }
-        
+
         public static void L9_P_EX_2_2()
         {
-            DateTime date = new DateTime(2018, 7, 20, 18, 30, 25);
-            int (year, month, day, hours, minutes, seconds) = date;
-            Console.WriteLine(year);
+            DateTime date = new DateTime();
+            var (_, _, _, hours, minute, second) = date;
+            Console.WriteLine($"{hours} {minute} {second}");
+        }
+        public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hours, out int minute, out int second)
+        {
+            year = DateTime.Now.Year; 
+            month = DateTime.Now.Month;
+            day = DateTime.Now.Day;
+            hours = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
         }
         ////DateTime date1 = new DateTime(2015, 7, 20, 18, 30, 25)
 
